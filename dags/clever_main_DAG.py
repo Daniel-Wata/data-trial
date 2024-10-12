0cd645254d320a7d8c7e0ebaadfd20da38cd025a
Daniel Watanabe <92555368+Daniel-Wata@users.noreply.github.com>
2024-10-12 11:12:02 -0300

    fixed the main dag

--- dags/clever_main_DAG.py
+++ dags/clever_main_DAG.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from scripts.clever_main_pipeline import upload_to_postgres
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
@@ -31,7 +32,7 @@ with DAG("clever_main_DAG", default_args=default_args, catchup=False, schedule_i
             task_id=task_id,
             python_callable=upload_to_postgres,
             dag=dag,
-            execution_timeout=timedelta(milliseconds=2),
+            execution_timeout=timedelta(minutes=10),
             op_kwargs={
                 "file_name": file
             }
